5f595331f2087ae38b0fd6b316a2b99dc35fd684
Suhho Lee <suhholeemichael@gmail.com>
2023-05-03 19:26:12 +0900

    working on about page

--- client/src/components/AboutMe.js
+++ client/src/components/AboutMe.js
@@ -1,8 +1,15 @@
 
 const AboutMe = () => {
   return (
-    <div id="about">
+    <div className="about">
       <h1>Who Am I?</h1>
+      <div className="content">
+        <p>I am a former sport science graduate and a current junior software engineer who strives to find meaningful work that positively influences the lives of others through technology.</p>
+        <p>The spark for a career change derived from my experience as a sport scientist at Oxford United FC, where I was able to understand that technology can shape how people work effectively in every industrial sector. My following role as a sport science and product team intern at QMIT gave me firsthand experience in how a product is developed in a tech company and how much teamwork is important in building the optimum product.</p>
+        <p>These experiences not only served as a foundation of understanding my skills and values, but also allowed me to acknowledge my passion in building innovative products and solutions to impact people’s lives positively.</p>
+        <p>Therefore, I decided to enroll in General Assembly’s Software Engineer Immersive which helped me gain strong knowledge in coding and a unique portfolio to officially pursue a career as a software engineer.</p>
+        <p>I inspire to be recognised individual in tech who can flexibly utilise the positives of the current norms to create an innovative product that can shape people’s lives in the future. At this stage of my career, I am looking to enhance my skills and further gain experience as a full-stack developer in a creative and collaborative team.</p>
+      </div>
     </div>
   )
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -14,6 +14,7 @@ const Home = () => {
       </div>
       <div className="profile">
         <img src={profile} className='profile-image'></img>
+        <div className='profile-wrapper'></div>
       </div>
     </div>
   )
